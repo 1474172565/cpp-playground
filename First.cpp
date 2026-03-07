c5c9d00bb08104d84f74929ccaa7c0d52d25263d
1474172565 <1474172565@qq.com>
2026-03-07 16:53:43 +0800

    second commit

--- First.cpp
+++ First.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
+#include <vector>
 
 int main()
 {
-    std::cout << "HELLO , WORLD";
+    int n{0};
+    std::cin >> n;
+    std::vector<int>FUCK (n , 6);
+    for (const auto &i : FUCK)
+    {
+        std::cout << i << '\t';
+    }
+    return 0;
 }
