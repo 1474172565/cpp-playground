2da21a50482654a22256d7aa162cb7444ce62e05
1474172565 <1474172565@qq.com>
2026-03-09 09:05:29 +0800

    It has a bug with erase_list

--- draft.cpp
+++ draft.cpp
@@ -8,7 +8,7 @@ struct Linked_list{
   std::unique_ptr<Linked_list> next;
 };
 
-void emplace_list(Linked_list &Head , const std::string &name ,const int num)
+void emplace_list(Linked_list &Head , const std::string &name ,const int num) // 在链表后加上新的链表（构造）；
 {
   Linked_list* current = &Head;
   while(current->next != nullptr)
@@ -21,31 +21,52 @@ void emplace_list(Linked_list &Head , const std::string &name ,const int num)
   current->next->num = num;
 }
 
-void erase_list(Linked_list &Head , const int index)
+void erase_list(Linked_list &Head , const int index) // 第 index 个链表之后删除
 {
   Linked_list* current = &Head;
-  for(int i = 0 ; i < index ; ++i)
-  {
-
+  for(int i = 0 ; i <= index ; ++i){
+    current = current->next.get();
   }
+
+  current->next.reset();
 }
 
-void Print_list(Linked_list &Head){
+void Print_list(Linked_list &Head){// 逐个打印
   Linked_list* current = &Head;
 
   while(current != nullptr)
   {
-    std::cout << current->name << current->num << '\n';
+    std::cout << '[' <<current->name << ',' << current->num << ']' << '\n';
     current = current->next.get();
   }
 }
 
+void Push_back(Linked_list &Head , Linked_list &B)//把链表 放到 最后 （赋值）
+{
+  Linked_list* current = &Head;
+  Linked_list* b = &B;
+
+  while(current->next != nullptr)
+  {
+    current = current->next.get();
+  }
+
+  current->next.reset(b);
+}
+
 int main()
 {
-  Linked_list A{"hahaha" , 1};
-  emplace_list(A , "asdw" , 2);
-
+  Linked_list A{"105" , 1};
+  Linked_list B{"AKA" , 123};
+  emplace_list(A , "106" , 2);
+  emplace_list(A , "107" , 3);
+  emplace_list(A , "108" , 4);
+  Push_back(A, B);
   Print_list(A);
 
+  erase_list(A, 2);
+
+  std::cout << '\n';
+  Print_list(A);
   return 0;
 }
